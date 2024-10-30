c367e596c106dcb7e5f735beb6483cc28d9bc154
chrisvobi <christoforidis02@gmail.com>
2024-10-31 00:47:45 +0200

    code for thema 1 complete

--- 1/simulation/thema_1.m
+++ 1/simulation/thema_1.m
@@ -2,12 +2,12 @@ clear all;
 close all;
 clc;
 
-% functions
+%% functions
 f1 = @(x) (x-2)^2 + x*log(x+3);
 f2 = @(x) exp(-2*x) + (x-2)^2;
 f3 = @(x) exp(x)*(x^3 - 1) + (x-1) * sin(x);
 
-% constant l variable e
+%% constant l variable e
 size = 50;
 k = zeros(size,1); e=zeros(size,1);
 
@@ -47,7 +47,7 @@ xlabel('\epsilon');
 ylabel('Number of iterations');
 title('$f_3$ = $e^{x}$ $\cdot$ $(x^{3}-1)$ + $(x-1)$ $\cdot$ $\sin{x}$','Interpreter', 'latex');
 
-% constant e variable l
+%% constant e variable l
 k = zeros(size,1); l=zeros(size,1);
 
 % f1
@@ -85,3 +85,140 @@ plot(l,k);
 xlabel('l');
 ylabel('Number of iterations');
 title('$f_3$ = $e^{x}$ $\cdot$ $(x^{3}-1)$ + $(x-1)$ $\cdot$ $\sin{x}$','Interpreter', 'latex');
+
+%% [ak, bk] for various l
+
+% f1, l = 0.0025
+epsilon = 0.001;
+[a,b,k,e,l] = bisection(f1,epsilon,0.0025);
+figure(7);
+hold on;
+for i=1:1:k
+    plot(i,a(i),'bo');
+    plot(i,b(i),'r*')
+end
+hold off;
+title('$f_1$ = $(x-2)^{2}$ + $x$ $\cdot$ $\ln{(x+3)}$','Interpreter', 'latex');
+xlabel('$k$ while $l$ = 0.0025','Interpreter','latex');
+ylabel('[$a_{k}$,$b_{k}$]','Interpreter','latex')
+legend({'$a_{k}$', '$b_{k}$'},'Interpreter','latex');
+
+% f1, l = 0.02
+epsilon = 0.001;
+[a,b,k,e,l] = bisection(f1,epsilon,0.02);
+figure(8);
+hold on;
+for i=1:1:k
+    plot(i,a(i),'bo');
+    plot(i,b(i),'r*')
+end
+hold off;
+title('$f_1$ = $(x-2)^{2}$ + $x$ $\cdot$ $\ln{(x+3)}$','Interpreter', 'latex');
+xlabel('$k$ while $l$ = 0.02','Interpreter','latex');
+ylabel('[$a_{k}$,$b_{k}$]','Interpreter','latex')
+legend({'$a_{k}$', '$b_{k}$'},'Interpreter','latex');
+
+% f1, l = 0.05
+epsilon = 0.001;
+[a,b,k,e,l] = bisection(f1,epsilon,0.05);
+figure(9);
+hold on;
+for i=1:1:k
+    plot(i,a(i),'bo');
+    plot(i,b(i),'r*')
+end
+hold off;
+title('$f_1$ = $(x-2)^{2}$ + $x$ $\cdot$ $\ln{(x+3)}$','Interpreter', 'latex');
+xlabel('$k$ while $l$ = 0.05','Interpreter','latex');
+ylabel('[$a_{k}$,$b_{k}$]','Interpreter','latex')
+legend({'$a_{k}$', '$b_{k}$'},'Interpreter','latex');
+
+% f2, l = 0.0025
+epsilon = 0.001;
+[a,b,k,e,l] = bisection(f2,epsilon,0.0025);
+figure(10);
+hold on;
+for i=1:1:k
+    plot(i,a(i),'bo');
+    plot(i,b(i),'r*')
+end
+hold off;
+title('$f_2$ = $e^{-2x}$ + $(x-2)^{2}$','Interpreter', 'latex');
+xlabel('$k$ while $l$ = 0.0025','Interpreter','latex');
+ylabel('[$a_{k}$,$b_{k}$]','Interpreter','latex')
+legend({'$a_{k}$', '$b_{k}$'},'Interpreter','latex');
+
+% f2, l = 0.02
+epsilon = 0.001;
+[a,b,k,e,l] = bisection(f2,epsilon,0.02);
+figure(11);
+hold on;
+for i=1:1:k
+    plot(i,a(i),'bo');
+    plot(i,b(i),'r*')
+end
+hold off;
+title('$f_2$ = $e^{-2x}$ + $(x-2)^{2}$','Interpreter', 'latex');
+xlabel('$k$ while $l$ = 0.02','Interpreter','latex');
+ylabel('[$a_{k}$,$b_{k}$]','Interpreter','latex')
+legend({'$a_{k}$', '$b_{k}$'},'Interpreter','latex');
+
+% f2, l = 0.05
+epsilon = 0.001;
+[a,b,k,e,l] = bisection(f2,epsilon,0.05);
+figure(12);
+hold on;
+for i=1:1:k
+    plot(i,a(i),'bo');
+    plot(i,b(i),'r*')
+end
+hold off;
+title('$f_2$ = $e^{-2x}$ + $(x-2)^{2}$','Interpreter', 'latex');
+xlabel('$k$ while $l$ = 0.05','Interpreter','latex');
+ylabel('[$a_{k}$,$b_{k}$]','Interpreter','latex')
+legend({'$a_{k}$', '$b_{k}$'},'Interpreter','latex');
+%%
+% f3, l = 0.0025
+epsilon = 0.001;
+[a,b,k,e,l] = bisection(f3,epsilon,0.0025);
+figure(13);
+hold on;
+for i=1:1:k
+    plot(i,a(i),'bo');
+    plot(i,b(i),'r*')
+end
+hold off;
+title('$$f_3$ = $e^{x}$ $\cdot$ $(x^{3}-1)$ + $(x-1)$ $\cdot$ $\sin{x}$','Interpreter', 'latex');
+xlabel('$k$ while $l$ = 0.0025','Interpreter','latex');
+ylabel('[$a_{k}$,$b_{k}$]','Interpreter','latex')
+legend({'$a_{k}$', '$b_{k}$'},'Interpreter','latex');
+
+% f3, l = 0.02
+epsilon = 0.001;
+[a,b,k,e,l] = bisection(f3,epsilon,0.02);
+figure(14);
+hold on;
+for i=1:1:k
+    plot(i,a(i),'bo');
+    plot(i,b(i),'r*')
+end
+hold off;
+title('$$f_3$ = $e^{x}$ $\cdot$ $(x^{3}-1)$ + $(x-1)$ $\cdot$ $\sin{x}$','Interpreter', 'latex');
+xlabel('$k$ while $l$ = 0.02','Interpreter','latex');
+ylabel('[$a_{k}$,$b_{k}$]','Interpreter','latex')
+legend({'$a_{k}$', '$b_{k}$'},'Interpreter','latex');
+
+% f3, l = 0.05
+epsilon = 0.001;
+[a,b,k,e,l] = bisection(f3,epsilon,0.05);
+figure(15);
+hold on;
+for i=1:1:k
+    plot(i,a(i),'bo');
+    plot(i,b(i),'r*')
+end
+hold off;
+title('$$f_3$ = $e^{x}$ $\cdot$ $(x^{3}-1)$ + $(x-1)$ $\cdot$ $\sin{x}$','Interpreter', 'latex');
+xlabel('$k$ while $l$ = 0.05','Interpreter','latex');
+ylabel('[$a_{k}$,$b_{k}$]','Interpreter','latex')
+legend({'$a_{k}$', '$b_{k}$'},'Interpreter','latex');
