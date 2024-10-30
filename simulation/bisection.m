fb4bf098be65b29ac1ce9fb21905c970ac9d1a53
chrisvobi <christoforidis02@gmail.com>
2024-10-30 22:59:46 +0200

    bisection function

---
+++ simulation/bisection.m
@@ -0,0 +1,18 @@
+function [a,b,k,e,l] = bisection(f,epsilon,lamda)
+l = lamda; e = epsilon;
+a = []; b = [];
+k = 1;
+a(k) = -1; b(k) = 3;
+ 
+while b(k) - a(k) >= l
+    x_1 = (a(k)+b(k))/2 - e;
+    x_2 = (a(k)+b(k))/2 + e;
+    if subs(f,x_1) < subs(f,x_2)
+        a(k+1) = a(k);
+        b(k+1) = x_2;
+    else
+        a(k+1) = x_1;
+        b(k+1) = b(k);
+    end
+    k = k + 1;
+end
--- simulation/thema_1.m
+++ simulation/thema_1.m
@@ -1,4 +1,7 @@
 clear all;
 clc;
 
-%%
+% functions
+f1 = @(x) (x-2)^2 + x*log(x+3);
+f2 = @(x) exp(-2*x) + (x-2)^2;
+f3 = @(x) exp(x)*(x^3 - 1) + (x-1) * sin(x);
